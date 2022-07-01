7845bd43070862d967d3b8ec4dfd0996de0b4eda
SERGY <kogo_sadgi@mail.ru>
2022-07-01 13:27:20 +0300

    Sem003

---
+++ Sem003/Program.cs
@@ -0,0 +1,17 @@
+﻿int randomNumder(int min, int max)
+{
+    int number = new Random().Next(min, max+1);
+    Console.Write(number+ "-> ");
+    int num1 = number / 10;
+    int num2 = number % 10;
+    //Console.WriteLine(number + " " + num1 + " " + num2);
+    if (num1 > num2)
+        return num1;
+    else
+        return num2;
+}
+Console.Write("Введите минимальное число диапазона: ");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное число диапазона: ");
+int end = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("максимальная цифра из случайного двухзначного числа: " + randomNumder(start, end));
