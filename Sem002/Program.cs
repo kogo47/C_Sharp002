9d5d0249f5eefef3ca91805d0f67d23def45cb93
SERGY <kogo_sadgi@mail.ru>
2022-07-02 13:46:03 +0300

    Sem002 amendments

--- Sem002/Program.cs
+++ Sem002/Program.cs
@@ -1,19 +1,76 @@
-﻿void randomNumder()
-{
-    int number = new Random().Next(10, 100);
-    Console.Write(number+ "->");
-    int num1 = number /10;
-    int num2 = number %10;
-    //Console.WriteLine(number + " " + num1 + " " + num2);
-    if (num1>num2)
-        {Console.WriteLine(num1);}
-    else
-        {Console.WriteLine(num1);}
+﻿// //переаброзуем предыдущий Sem001 в метод void
+// void randomNumder()
+// {
+//     int number = new Random().Next(10, 100);
+//     Console.Write(number+ "->");
+//     int num1 = number /10;
+//     int num2 = number %10;
+//     if (num1>num2)
+//         {Console.WriteLine(num1);}
+//     else
+//         {Console.WriteLine(num1);}
 
-    return;
+//     return;
+// }
+// Console.WriteLine("Введите минимальное число диапазона: ");
+// int start = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимальное число диапазона: ");
+// int end = Convert.ToInt32(Console.ReadLine());
+// randomNumber(start, end);
+
+
+// void randomNumder(){
+//    int number = new Random().Next(10, 100);Console.WriteLine(number);
+// Console.Write(number + "-> "); 
+// if (number / 10 > number % 10){//если num1 будет больше num2
+//     Console.WriteLine(number / 10);// то тогда тогда выводим num1 
+// }
+// else//иначе, если num1 не больше num2 то тода-
+// {
+//    Console.WriteLine(number % 10);//выводим num2
+// }
+// }
+// for (int i = 0; i < 10; i++)//вызов цикла
+// {
+//     randomNumder();
+// }
+
+
+
+// // первый способ объявить метод void
+//     //название метода
+// void randomNumder(){
+//    int number = new Random().Next(10, 100);Console.WriteLine(number);
+// Console.Write(number + "-> "); 
+// if (number / 10 > number % 10){//если num1 будет больше num2
+//     Console.WriteLine(number / 10);// то тогда тогда выводим num1 
+// }
+// else//иначе, если num1 не больше num2 то тода-
+// {
+//    Console.WriteLine(number % 10);//выводим num2
+// }
+// }
+//     randomNumder();
+
+
+
+
+//второй способ объявить метод void
+//     //название метода
+void randomNumder(int min, int max){//принимает свои значения переменных
+   int number = new Random().Next(min, max+1);Console.WriteLine(number);
+Console.Write(number + "-> "); 
+if (number / 10 > number % 10){//если num1 будет больше num2
+    Console.WriteLine(number / 10);// то тогда тогда выводим num1 
+}
+else//иначе, если num1 не больше num2 то тода-
+{
+   Console.WriteLine(number % 10);//выводим num2
+}
 }
 Console.WriteLine("Введите минимальное число диапазона: ");
 int start = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите максимальное число диапазона: ");
 int end = Convert.ToInt32(Console.ReadLine());
-randomNumber(start, end);
+
+randomNumder(start, end);//передаем аргумента переменных методу
